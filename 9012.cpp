1f91f56569471feed9bbea9a1bf368b7b283aeef
chanhong9764 <chanhong9784@naver.com>
2019-07-13 14:16:57 +0000

    #9012

---
+++ 9012.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    stack <char> s;
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+    {
+        string input;
+        cin >>input;
+        
+        for(int j=0; j < input.size(); j++)
+        {
+            if(input[j] == '(')
+            {
+                s.push(input[j]);
+            }
+            else if(input[j] == ')')
+            {
+                if(s.empty())
+                    s.push(input[j]);
+                else
+                	s.pop();
+            }
+            else
+                cout << "Error !" <<endl;
+        }
+        if(s.empty())
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+        while(!s.empty())
+            s.pop();
+    }
+    return 0;
+}
